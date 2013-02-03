a8a24b4b42991c2c92b9b1fa7c51759e6fac82ba
ikst <igzarion.1993@gmail.com>
2013-02-03 23:22:58 +0900

    change title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
   def page_title
     title = "BEATECH"
-    title = title + " - " + @page_title if @page_title
+    title = title + " - " + @page.title if @page.title
+    if @page.url == "frontpage"
+      title = "東工大音楽ゲーム愛好会BEATECH"
+    end
     title
   end
 end
