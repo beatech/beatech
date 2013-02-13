35285a01234eb5cca8f8c71f72d9ffbd275942ff
ikst <igzarion.1993@gmail.com>
2013-02-14 01:59:50 +0900

    complete contest page

--- app/controllers/contest2nds_controller.rb
+++ app/controllers/contest2nds_controller.rb
@@ -6,6 +6,50 @@ class Contest2ndsController < ApplicationController
     @a_team = Contest2nd.find(:all, :conditions => {:team => 'A'})
     @b_team = Contest2nd.find(:all, :conditions => {:team => 'B'})
     @c_team = Contest2nd.find(:all, :conditions => {:team => 'C'})
+
+    @a_total_score = 0
+    @b_total_score = 0
+    @c_total_score = 0
+
+    @a_total_rate = 0
+    @b_total_rate = 0
+    @c_total_rate = 0
+
+    @a_total_bp = 0
+    @b_total_bp = 0
+    @c_total_bp = 0
+
+    # 合計計算
+    @a_team.each do |a|
+      total_score = a.a_score + a.b_score + a.c_score
+      a["total_score"] = total_score
+      @a_total_score += total_score
+      
+      total_bp = a.a_bp + a.b_bp + a.c_bp
+      a["total_bp"] = total_bp
+      @a_total_bp += total_bp
+    end
+
+    @b_team.each do |b|
+      total_score = b.a_score + b.b_score + b.c_score
+      b["total_score"] = total_score
+      @b_total_score += total_score
+      
+      total_bp = b.a_bp + b.b_bp + b.c_bp
+      b["total_bp"] = total_bp
+      @a_total_bp += total_bp
+    end
+
+    @c_team.each do |c|
+      total_score = c.a_score + c.b_score + c.c_score
+      c["total_score"] = total_score
+      @c_total_score += total_score
+      
+      total_bp = c.a_bp + c.b_bp + c.c_bp
+      c["total_bp"] = total_bp
+      @c_total_bp += total_bp
+    end
+    
     @title = '第二回部内大会'
   end
 
@@ -21,18 +65,19 @@ class Contest2ndsController < ApplicationController
   end
 
   def tunesupdate
-    @contest2nd = Contest2nd.find(params[:id])
-
-    respond_to do |format|
-      if @contest2nd.update_attributes(params[:contest2nd])
-        format.html { redirect_to @contest2nd, :notice => 'Contest2nd was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @contest2nd.errors, :status => :unprocessable_entity }
-      end
+    @tunes = params[:tune]
+    @tunes.each do |tune|
+      data = tune[1]
+      @order = data["order"]
+      @team = data["team"]
+      
+      contests = Contest2nd.find(:all, :conditions => {:team => @team, :order => @order})
+      contest = contests[0]
+      contest.notes = data["notes"]
+      contest.music = data["music"]
+      contest.save
     end
-    
+    redirect_to root_url + 'contest2nd', :notice => '曲名の編集に成功しました。'
   end
   
   # GET /contest2nds
@@ -97,7 +142,7 @@ class Contest2ndsController < ApplicationController
 
     respond_to do |format|
       if @contest2nd.update_attributes(params[:contest2nd])
-        format.html { redirect_to @contest2nd, :notice => 'Contest2nd was successfully updated.' }
+        format.html { redirect_to root_url + 'contest2nd', :notice => '対戦結果の編集に成功しました。' }
         format.json { head :no_content }
       else
         format.html { render :action => "edit" }
