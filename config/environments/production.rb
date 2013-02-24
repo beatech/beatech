b90dee4f44d4a6852aab9d2e178e8a178539cb92
ikst <igzarion.1993@gmail.com>
2013-02-25 00:07:06 +0900

    bugfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Beatech::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.debug = true
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
