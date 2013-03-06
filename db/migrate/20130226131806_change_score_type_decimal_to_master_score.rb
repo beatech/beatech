6a9f8782407c1d6183a9c214c4f95e86012f8de0
ikstrm <igzarion.1993@gmail.com>
2013-03-06 22:14:37 +0900

    Remove needless migrations

--- db/migrate/20130226131806_change_score_type_decimal_to_master_score.rb
+++
@@ -1,9 +0,0 @@
-class ChangeScoreTypeDecimalToMasterScore < ActiveRecord::Migration
-  def up
-    change_column :master_scores, :score, :decimal
-  end
-
-  def down
-    change_column :master_scores, :score, :float
-  end
-end
--- db/migrate/20130226140018_change_score_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeScoreType < ActiveRecord::Migration
-  def up
-    change_column :master_scores, :score, :decimal, :precision => 8, :scale => 2
-  end
-
-  def down
-    change_column :master_scores, :score, :decimal
-  end
-end
