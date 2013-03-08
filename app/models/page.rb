081d339ac02c602c8d38b4fe14988cc0e12f7bbe
ikstrm <igzarion.1993@gmail.com>
2013-03-09 01:39:45 +0900

    Make grammer for making tabs

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,13 @@
 class Page < ActiveRecord::Base
   attr_accessible :limit, :menu, :text, :title, :url
+
+  def self.title_by_url(url)
+    @page = Page.find_by_url(url)
+    
+    if @page
+      @page.title
+    else
+      nil
+    end
+  end
 end
