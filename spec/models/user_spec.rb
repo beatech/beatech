1ffba00b527a12f681d71d52870551a7ba398bb1
ikstrm <mail@ikst.jp>
2013-04-16 20:47:54 +0900

    Validate account

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe User do
+  describe '.create' do
+    it "should not create user whose account name already exists" do
+      2.times do
+        User.create(
+          name: 'hoge',
+          account: 'hoge',
+          password: 'hoge',
+          password_confirmation: 'hoge',
+          year: 2013,
+          repeat_year: 2013
+        )
+      end
+      User.where(account: 'hoge').size.should == 1
+    end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:account) }
+    it 'should validate account'
+  end
+end
