cbbb7f1ad30403422648a3916038adc9f2ac0b2e
kokubun <takashi_kokubun@tekunodo.jp>
2013-05-24 10:16:52 +0900

    Add test for pages_controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,7 @@ describe PagesController do
       pages_controller = PagesController.new
       pages_controller.convert_wiki("//aaaaaa").gsub(/<.+?>/, "").should == ""
       pages_controller.convert_wiki("aaa//aaaaaa").gsub(/<.+?>/, "").should == "aaa"
+      pages_controller.convert_wiki("ち//く//わ//大//明//神").gsub(/<.+?>/, "").should == "ち"
     end
   end
 end
