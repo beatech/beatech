f3750c2df855e4e840006f59964a80eef46a4fba
kokubun <kokubun.t.aa@m.titech.ac.jp>
2013-08-08 01:37:50 +0900

    Generate entries_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Beatech::Application.routes.draw do
+  get "entries/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
