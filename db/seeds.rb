8c7d9572a90f8e7927b719032573e5f257782f2c
kokubun <kokubun.t.aa@m.titech.ac.jp>
2013-08-13 00:24:31 +0900

    Write seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,16 @@
-# require 'open-uri'
-# require 'json'
-# require 'pp'
+require 'open-uri'
+require 'json'
 
-# pages = JSON.parse(open("http://beatech.net/pages.json").read)
-# pages.each do |page|
-#   puts page["title"]
-#   pp page
-#   Entry.create(
-#     title: page["title"],
-#     url: page["url"],
-#     #menu: page["menu"],
-#     text: page["text"],
-#     menu: 0,
-#     type: 3,
-#     #type: page["limit"],
-#   )
-# end
+Entry.all.each { |entry| entry.destroy }
+pages = JSON.parse(open("http://beatech.net/pages.json").read)
+pages.each do |page|
+  puts page["title"]
+  Entry.create(
+    title: page["title"],
+    url: page["url"],
+    menu: page["menu"],
+    text: page["text"],
+    entry_type: page["limit"],
+  )
+end
 
-Entry.create(title: "a", url: "a", text: "a", menu: 0, type: 0)
