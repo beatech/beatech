cff52ef7030a20f963b96456617ba51813d8489a
kokubun <kokubun.t.aa@m.titech.ac.jp>
2013-08-12 19:43:16 +0900

    Fix migration for TeamGame/Music

--- db/migrate/20130812081609_create_team_game_music_scores.rb
+++ db/migrate/20130812081609_create_team_game_music_scores.rb
@@ -2,6 +2,7 @@ class CreateTeamGameMusicScores < ActiveRecord::Migration
   def change
     create_table :team_game_music_scores do |t|
       t.string :score
+      t.references :music, index: true
       t.references :user_score, index: true
       t.references :score_type, index: true
 
