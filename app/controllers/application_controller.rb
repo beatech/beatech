5c7583d9e44cea2b8aa84234a85f5853bfc7e234
kokubun <kokubun.t.aa@m.titech.ac.jp>
2013-08-13 09:37:18 +0900

    Load entries for menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :load_entries
+
+  def load_entries
+    @circle_menu = Entry.where(menu: 1)
+    @communication_menu = Entry.where(menu: 2)
+    @contents_menu = Entry.where(menu: 3)
+    @header_menu = Entry.where(menu: 4)
+
+    @recent_pages = Entry.all
+    @recent_pages.sort! { |a, b| b.updated_at <=> a.updated_at }
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  controller do
+    def index
+      render :text => "index called"
+    end
+  end
+
+  describe "#load_entries" do
+    it "should assign entries to instance valiables" do
+      get :index
+      assigns(:circle_menu).should be_a_kind_of(ActiveRecord::Relation::ActiveRecord_Relation_Entry)
+      assigns(:communication_menu).should be_a_kind_of(ActiveRecord::Relation::ActiveRecord_Relation_Entry)
+      assigns(:contents_menu).should be_a_kind_of(ActiveRecord::Relation::ActiveRecord_Relation_Entry)
+      assigns(:header_menu).should be_a_kind_of(ActiveRecord::Relation::ActiveRecord_Relation_Entry)
+      assigns(:recent_pages).should be_a_kind_of(ActiveRecord::Relation::ActiveRecord_Relation_Entry)
+    end
+  end
+end
