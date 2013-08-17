67f9dd890c748e5b9fbf08702e59382ae25379b8
kokubun <kokubun.t.aa@m.titech.ac.jp>
2013-08-18 00:12:33 +0900

    Update APIs for entries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 module ApplicationHelper
+  def admin_required
+    raise Exception unless is_admin?
+  end
+
+  def is_admin?
+    return false unless @current_user
+    admins = %w|ikstrm popkirby|
+    admins.each do |admin|
+      return true if @current_user.username == admin
+    end
+    false
+  end
+
   def title_with_entry(url)
     if url == 'frontpage'
       '東工大音ゲーサークルBEATECH'
@@ -14,6 +27,45 @@ module ApplicationHelper
       link_to(title, url)
     else
       link_to(title, root_url + url)
+    end
+  end
+  
+  def edit_link
+    if @entry && @entry.url
+      tabs = tabs(@entry.content)
+      if tabs
+        link = '<li><div class="dropdown">'
+        link += '<a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text-dropshadow">編集 <span class="caret" style="border-top:solid 4px #FFF; margin-top:12px;"></span></a>'
+        link += '<ul class="dropdown-menu"><li style="width: 160px;">'
+        link += link_to @entry.title, edit_entry_path(@entry)
+
+        tabs.each do |tab|
+          link += '</li><li style="width: 160px;">'
+          link += link_to Entry.find_by_url(tab).title, root_url + "edit/" + tab
+        end
+        
+        link += '</li></ul></div></li>'
+        link        
+      else
+        link = '<li>'
+        link += link_to "編集", edit_entry_path(@entry)
+        link += '</li>'
+        link
+      end
+    else
+      ''
+    end
+  end
+
+  def tabs(text)
+    if text
+      if text =~/&tab\(.+\)/
+        $&.gsub(/^&tab\(/, "").gsub(/\)/, "").strip.split(",")
+      else
+        nil
+      end
+    else
+      nil
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Beatech::Application.routes.draw do
-  #
-  # Session
-  #
   resource :session, only: [:create, :destroy]
 
-  #
-  # Entries
-  #
   resources :entries, only: [:index, :new, :create]
-  resources :entries, constraints: { id: /[^\/]+/ }, except: [:index, :new, :create], path: "/"
+  resources :entries, constraints: { id: /.+/ }, except: [:index, :new, :create], path: "/"
   
   root to: "entries#frontpage"
 end
