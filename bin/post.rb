3ca3f18b945d594ec49a41180beaf674f9f78cbb
Takashi Kokubun <takashikkbn@gmail.com>
2013-09-28 20:09:20 +0900

    Add achievement post script

---
+++ bin/post.rb
@@ -0,0 +1,11 @@
+@twitter_account = TwitterAccount.find_by_uid(ARGV[2].to_i) if ARGV[2]
+
+if @twitter_account && ARGV[3] && ARGV[3].length > 0
+  @achievement = Achievement.new
+  @achievement.user_id = @twitter_account.user_id
+  @achievement.text = ARGV[3] if ARGV[3]
+  @achievement.date = Date.today
+  @achievement.save
+end
+
+Entry.find_by_url('achievements').touch
