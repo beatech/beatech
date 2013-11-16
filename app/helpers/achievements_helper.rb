65b132db5a9ac20be0ac077db1f9e0cd41d9e418
Takashi Kokubun <takashikkbn@gmail.com>
2013-11-17 00:27:03 +0900

    Fix achievement converter

--- app/helpers/achievements_helper.rb
+++ app/helpers/achievements_helper.rb
@@ -13,15 +13,15 @@ module AchievementsHelper
     [latest_date, latest_date_achievements, rest_achievements]
   end
 
-  def achievement_text(achievement)
-    achievement.text
-  end
-
   def achievement_profile_image(achievement)
     if achievement.user && achievement.user.profile_image
       achievement.user.profile_image
     else
       'https://si0.twimg.com/sticky/default_profile_images/default_profile_0_normal.png'
     end
+  end
+
+  def autolink(text)
+    text.gsub!(/([^'"])(https?:\/\/[0-9a-zA-Z.\/?=&_]+)/, '\1<a href="\2">\2</a>')
   end
 end
