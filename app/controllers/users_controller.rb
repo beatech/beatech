a2e4bf4fdc692a85c7a35d55454696592270749e
Takashi Kokubun <takashikkbn@gmail.com>
2014-01-12 22:02:41 +0900

    Route user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  AVAILABLE_SETTING_ITEMS = %w(account password profile)
+
   def index
     @users_by_grade = User.all.group_by(&:grade)
     @user_count_by_grade = User.group(:grade).count
@@ -9,5 +11,27 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_username(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+    redirect_to root_url unless @login_user
+    unless AVAILABLE_SETTING_ITEMS.include?(params[:item])
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
+  def update
+    redirect_to root_url unless @login_user
+  end
+
+  def destroy
+    redirect_to root_url unless @login_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Beatech::Application.routes.draw do
   resource :session, only: [:create, :destroy]
 
-  resources :users, only: [:index, :create, :new]
-  resources :users, only: [:show, :update], path: '/', constraints: { id: /[a-zA-Z0-9_.]{1,15}/ }
+  resources :users, only: [:index]
+  resources :users, only: [:show], path: '/', constraints: { id: /[a-zA-Z0-9_.]{1,15}/ } do
+    collection do
+      get  :registration, action: 'new'
+      post :registration, action: 'create'
+      post :deactivate,   action: 'destroy'
+    end
+  end
+  resource :user, only: [], path: 'settings', constraints: { item: /[a-z]+/ } do
+    get  '/:item', action: 'edit', as: 'setting'
+    post '/:item', action: 'update'
+  end
 
   root to: 'tops#index'
 end
