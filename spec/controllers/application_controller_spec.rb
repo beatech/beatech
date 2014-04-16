7b4c6ec6ae2647162085c4b854bbf0e05c24489b
Takashi Kokubun <takashikkbn@gmail.com>
2014-04-17 00:12:19 +0900

    Kill double quotes

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe ApplicationController do
   controller do
     def index
-      render text: "index called"
+      render text: 'index called'
     end
   end
 
-  describe "#load_entries" do
-    it "should assign entries to instance valiables" do
+  describe '#load_entries' do
+    it 'should assign entries to instance valiables' do
       get :index
       assigns(:circle_menu).should be_a_kind_of(ActiveRecord::Relation::ActiveRecord_Relation_Entry)
       assigns(:communication_menu).should be_a_kind_of(ActiveRecord::Relation::ActiveRecord_Relation_Entry)
--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -4,21 +4,21 @@ describe EntriesController do
   render_views
   fixtures :entries
 
-  describe "GET 'frontpage'" do
-    it "should render #show" do
+  describe 'GET "frontpage"' do
+    it 'should render #show' do
       get 'frontpage'
       response.should be_success
-      response.should render_template("show")
+      response.should render_template('show')
     end
   end
 
-  describe "#show" do
-    it "should render a specific entry" do
+  describe '#show' do
+    it 'should render a specific entry' do
       get :frontpage
-      response.should contain("frontpage is rendered")
+      response.should contain('frontpage is rendered')
     end
 
-    it "should be able to render entries which contain tabs" do
+    it 'should be able to render entries which contain tabs' do
       get :show, id: 'tips'
       response.should be_success
     end
