916b7d8a96a3e113301d1d28c9e98b95cbbe93f6
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-08 00:28:23 +0900

    Use dotenv for consumer

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,10 +31,10 @@ class User < ActiveRecord::Base
 
   def self.update_profile_images
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key = Beatech::CONSUMER_KEY
-      config.consumer_secret = Beatech::CONSUMER_SECRET
-      config.access_token = Beatech::ACCESS_TOKEN
-      config.access_token_secret = Beatech::ACCESS_TOKEN_SECRET
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
     end
 
     User.all.each do |user|
