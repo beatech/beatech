d275a8ac0d4fc8f43691dde198a0653897700abe
popkirby <popkirby@gmail.com>
2014-12-22 20:57:50 +0900

    change settings for automated deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :branch, 'master'
 set :rails_env, 'production'
 set :migration_role, 'db'
 
-server 'beatech', user: 'beatech', roles: %w{web app db}
+server 'beatech.net', user: 'beatech', port: '8931', roles: %w{web app db}
